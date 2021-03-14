f8d456a922285c42f5af2599b5c0fae4ce784e73
ollykapow <hellokapow@gmail.com>
2021-03-14 15:10:41 +0000

    Removed styled components example code.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Button = styled.button`
-  cursor: pointer;
-  background: transparent;
-  font-size: 16px;
-  color: ${props => (props.primary ? 'cornflowerblue' : 'black')};
-  border:  2px solid  ${props => (props.primary ? 'cornflowerblue' : 'black')};
-  margin: 0;
-  padding: 0.25em 1em;
-  transition: 0.5s all ease-out;
-
-  &:hover,
-  &:active {
-    background-color: ${props => (props.primary ? 'cornflowerblue' : 'black')};
-    color: white;
-  }
-`;
 
 function App () {
   return (
@@ -24,12 +6,6 @@ function App () {
       <h1>
         Hello, World!
       </h1>
-      <Button onClick={() => alert('Clicked.')}>
-        Click me!
-      </Button>
-      <Button primary onClick={() => alert('Clicked.')}>
-        Click me!
-      </Button>
     </>
   );
 }
